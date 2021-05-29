f313a62be8e14297d25d43e18a1a9b020d770d7b
Ronuk Raval <ronuk.raval@gmail.com>
2021-05-29 12:18:56 -0400

    Document command

--- src/command.rs
+++ src/command.rs
@@ -1,3 +1,5 @@
+//! High level user invoked workflows for nomad.
+
 use anyhow::{bail, Result};
 
 use crate::{
@@ -5,6 +7,9 @@ use crate::{
     progress::Progress,
 };
 
+/// Initialize a git clone to have branches managed by nomad.
+///
+/// Will refuse to overwrite an already existing configuration.
 pub fn init<B: Backend>(progress: &Progress, backend: B, new_config: &Config) -> Result<()> {
     if let Some(existing_config) = backend.read_config()? {
         bail!(
@@ -21,6 +26,7 @@ pub fn init<B: Backend>(progress: &Progress, backend: B, new_config: &Config) ->
     Ok(())
 }
 
+/// Synchronize current local branches with nomad managed refs in the given remote.
 pub fn sync<B: Backend>(backend: B, config: &Config, remote: &Remote) -> Result<()> {
     backend.push(config, remote)?;
     let snapshot = backend.fetch(config, remote)?;

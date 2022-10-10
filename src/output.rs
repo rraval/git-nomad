24a897e3e598e1c2e56e3f1732b5af47da017bfe
Ronuk Raval <ronuk.raval@gmail.com>
2022-10-10 10:42:45 -0400

    Test `OutputStream`

--- src/output.rs
+++ src/output.rs
@@ -3,7 +3,7 @@ use std::io::{stdout, Stdout, StdoutLock, Write};
 /// An abstraction point around a [`Write`] implementation. Allows redirecting output for test
 /// assertions.
 pub struct OutputStream<Writer: Write, Data> {
-    sink: Writer,
+    writer: Writer,
     _data: Data,
 }
 
@@ -12,9 +12,9 @@ impl<'a> OutputStream<Stdout, StdoutLock<'a>> {
     ///
     /// Implicitly acquires [`StdoutLock`] since the whole application is single threaded.
     pub fn new_stdout() -> Self {
-        let sink = stdout();
-        let _data = sink.lock();
-        Self { sink, _data }
+        let writer = stdout();
+        let _data = writer.lock();
+        Self { writer, _data }
     }
 }
 
@@ -23,13 +23,13 @@ impl OutputStream<Vec<u8>, ()> {
     /// An [`OutputStream`] backed by memory.
     pub fn new_vec() -> Self {
         Self {
-            sink: Vec::new(),
+            writer: Vec::new(),
             _data: (),
         }
     }
 
     pub fn as_str(&self) -> &str {
-        std::str::from_utf8(self.sink.as_slice()).expect("tests should have utf8 output")
+        std::str::from_utf8(self.writer.as_slice()).expect("tests should have utf8 output")
     }
 }
 
@@ -38,7 +38,7 @@ impl OutputStream<std::io::Sink, ()> {
     /// An [`OutputStream`] that throws away all writes.
     pub fn new_sink() -> Self {
         Self {
-            sink: std::io::sink(),
+            writer: std::io::sink(),
             _data: (),
         }
     }
@@ -46,10 +46,39 @@ impl OutputStream<std::io::Sink, ()> {
 
 impl<Writer: Write, Data> Write for OutputStream<Writer, Data> {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        self.sink.write(buf)
+        self.writer.write(buf)
     }
 
     fn flush(&mut self) -> std::io::Result<()> {
-        self.sink.flush()
+        self.writer.flush()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::OutputStream;
+    use std::io::Write;
+
+    /// This test makes no assertions, merely getting a handle and locking it is sufficient.
+    #[test]
+    fn stdout_stream() {
+        OutputStream::new_stdout();
+    }
+
+    #[test]
+    fn vec_stream() {
+        let mut stream = OutputStream::new_vec();
+        write!(stream, "foo").unwrap();
+        assert_eq!(stream.as_str(), "foo");
+
+        // Flushing should be a no-op
+        stream.flush().unwrap();
+        assert_eq!(stream.as_str(), "foo");
+    }
+
+    #[test]
+    fn sink_stream() {
+        let mut stream = OutputStream::new_sink();
+        write!(stream, "foo").unwrap();
     }
 }
